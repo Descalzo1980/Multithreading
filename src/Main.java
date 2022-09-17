8ff48c39dea1eeccea49b97af9b8a3f5f89d15c3
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-09-17 20:39:24 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,34 @@
+
 public class Main {
-    static boolean isFive = false;
+    static boolean win = false;
 
     public static void main(String[] args) {
-        Thread timer = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    int i = 0;
-                    while (true){
-                        if(i == 5){
-                            isFive = true;
-                        }
-                        System.out.println(i);
-                        i++;
-                        Thread.sleep(1000);
-                    }
-                }catch (Exception e){
+        long random = (long) (Math.random() * 100000000);
+        System.out.println(random);
+        Thread timer = new Thread(() -> {
+            int i = 0;
+            try {
+                while (!win) {
+                    System.out.println(i);
+                    i++;
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
 
+        Thread player = new Thread(() -> {
+            while (!win) {
+                long guessNumber = (long) (Math.random() * 1_000_000_000);
+                if (guessNumber == random) {
+                    win = true;
+                    System.out.println(guessNumber);
                 }
             }
         });
         timer.start();
-        Thread timer2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    int i = 0;
-                    while (!isFive){
-                        System.out.println(i);
-                        i++;
-                        Thread.sleep(1000);
-                    }
-                }catch (Exception e){
-
-                }
-            }
-        });
-        timer2.start();
+        player.start();
     }
 
 }
